f7aa952dfdca60e152eaf34db5b3b795706d446b
Andrew Virya Victorio <andrew.avv03@gmail.com>
2026-02-07 11:26:43 +0700

    chore: use jetbrains mono font to root layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import './styles.css'
 import { Toaster } from '@/components/ui/sonner'
 import { ThemeProvider } from 'next-themes'
+import { JetBrains_Mono } from 'next/font/google'
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '600'],
+  adjustFontFallback: true,
+  style: 'normal',
+  variable: '--font-jetbrains-mono',
+  preload: true,
+  fallback: ['monospace'],
+})
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -12,7 +24,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={`${jetbrainsMono.variable}`}>
       <body>
         <main>
           <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
